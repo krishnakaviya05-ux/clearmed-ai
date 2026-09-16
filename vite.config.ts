b17e5c4d030526e1d22d00d02a8a41cbc872ba61
Kaviya <krishnakaviya05@gmail.com>
2026-09-17 00:09:29 +0530

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,15 @@ import { defineConfig } from 'vite';
 
 
 export default defineConfig({
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
+  base: '/clearmed-ai/',
+  server: {
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  plugins: [],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    }
+  }
 });
